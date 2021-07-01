a91db4abc4ad56ffba8138b5311b4b4f900f6e14
Harun Shaban <42933471+harunshaban@users.noreply.github.com>
2021-07-01 12:29:21 +0200

    registration success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { MainpageComponent } from './pages/mainpage/mainpage.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'mainpage', component: MainpageComponent },
 
 ];
 
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authState: any = null;
+
+  constructor( private afu: AngularFireAuth, private router: Router) 
+  {
+    this.afu.authState.subscribe((auth=>{
+      this.authState = auth;
+    }))
+  }
+
+  registerWithEmail(email: string, password: string)
+  {
+    this.afu.createUserWithEmailAndPassword(email, password).then((user)=>{
+      this.authState = user
+    }).catch(error=>{
+      console.log(error)
+      throw error
+    })
+  }
+}
